7a1cd1a5c2ee6886254e7e1400cd061996357021
Aleksey Fiodorov <afedorov@perfectpanel.com>
2022-12-15 16:57:58 +0300

    [ASYNC] - DI container

--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -3,26 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Parser;
-use Illuminate\Http\Request;
 use App\Models\Task;
 
-use Clue\React\Buzz\Browser;
 use React\EventLoop\Loop;
+use React\EventLoop\LoopInterface;
 
 class ParserController extends Controller
 {
     public function index()
     {
-        $loop = Loop::get();
-        $client = new Browser($loop);
+        app()->bind(LoopInterface::class, function (){
+            return Loop::get();
+        });
 
-        $parser = new Parser($client, $loop);
+        $parser = app()->make(Parser::class);
+
         $parser->parse([
             'https://start.ru/watch/vampiry-sredney-polosy?utm_source=kinopoisk_special&utm_medium=cpd&utm_campaign=vampiry-sredney-polosy_2_main_ru',
             'https://start.ru/watch/franshiza-posledniy-bogatyr/season-1',
         ], 3);
 
-        $loop->run();
+        app()->call(LoopInterface::class, [], 'run');
+
         var_dump($parser->getMovieData());
     }
 }
